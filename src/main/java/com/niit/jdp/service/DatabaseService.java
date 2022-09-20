d36a2c29155373817e2056db85c736baea95ff1a
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-20 17:41:40 +0530

    created no args constructor for Connection object

--- src/main/java/com/niit/jdp/service/DatabaseService.java
+++ src/main/java/com/niit/jdp/service/DatabaseService.java
@@ -6,8 +6,15 @@
  */
 package com.niit.jdp.service;
 
+import java.sql.Connection;
+
 public class DatabaseService {
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
     private static final String URL = "";
+    private Connection connection;
+
+    public DatabaseService() {
+        this.connection = null;
+    }
 }
