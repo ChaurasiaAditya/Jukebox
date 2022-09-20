8c521db933793f4b35dc1ea00a1be3bedc10621b
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-20 17:37:24 +0530

    created PlaylistRepository class

---
+++ src/main/java/com/niit/jdp/repository/PlaylistRepository.java
@@ -0,0 +1,10 @@
+/*
+ * Author Name: Aditya Chaurasia
+ * Date: 20-09-2022
+ * Created With: IntelliJ IDEA Community Edition
+ * Profile: github.com/ChaurasiaAditya
+ */
+package com.niit.jdp.repository;
+
+public class PlaylistRepository {
+}
