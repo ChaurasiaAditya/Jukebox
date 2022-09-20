c6929eac074f5ee9522471a1f31d64a783c1a3ef
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-20 23:37:05 +0530

    updated the to string method in song

--- src/main/java/com/niit/jdp/model/Song.java
+++ src/main/java/com/niit/jdp/model/Song.java
@@ -11,21 +11,21 @@ import java.util.Objects;
 public class Song {
     private int songNumber;
     private String songName;
-    private String songUrl;
     private double duration;
     private String artist;
     private String genre;
+    private String songUrl;
 
     public Song() {
     }
 
-    public Song(int songNumber, String songName, String songUrl, double duration, String artist, String genre) {
+    public Song(int songNumber, String songName, double duration, String artist, String genre, String songUrl) {
         this.songNumber = songNumber;
         this.songName = songName;
-        this.songUrl = songUrl;
         this.duration = duration;
         this.artist = artist;
         this.genre = genre;
+        this.songUrl = songUrl;
     }
 
     public int getSongNumber() {
@@ -42,14 +42,6 @@ public class Song {
 
     public void setSongName(String songName) {
         this.songName = songName;
-    }
-
-    public String getSongUrl() {
-        return songUrl;
-    }
-
-    public void setSongUrl(String songUrl) {
-        this.songUrl = songUrl;
     }
 
     public double getDuration() {
@@ -76,28 +68,35 @@ public class Song {
         this.genre = genre;
     }
 
+    public String getSongUrl() {
+        return songUrl;
+    }
+
+    public void setSongUrl(String songUrl) {
+        this.songUrl = songUrl;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Song song = (Song) o;
-        return songNumber == song.songNumber && Double.compare(song.duration, duration) == 0 && Objects.equals(songName, song.songName) && Objects.equals(songUrl, song.songUrl) && Objects.equals(artist, song.artist) && Objects.equals(genre, song.genre);
+        return songNumber == song.songNumber && Double.compare(song.duration, duration) == 0 && Objects.equals(songName, song.songName) && Objects.equals(artist, song.artist) && Objects.equals(genre, song.genre) && Objects.equals(songUrl, song.songUrl);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(songNumber, songName, songUrl, duration, artist, genre);
+        return Objects.hash(songNumber, songName, duration, artist, genre, songUrl);
     }
+
 
     @Override
     public String toString() {
-        return "Song{" +
-                "songNumber=" + songNumber +
-                ", songName='" + songName + '\'' +
-                ", songUrl='" + songUrl + '\'' +
-                ", duration=" + duration +
-                ", artist='" + artist + '\'' +
-                ", genre='" + genre + '\'' +
-                '}';
+        return  getSongNumber() +"  "+
+                getSongName() + '\'' +
+                getArtist() + '\'' +
+                getGenre() +
+                getDuration() + '\'' +
+                "\n";
     }
 }
