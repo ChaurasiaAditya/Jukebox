b139259ccb21ce3307543ff5b52a5c7f32548a1b
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-20 21:06:33 +0530

    added interface for Repository

--- src/main/java/com/niit/jdp/repository/Repository.java
+++ src/main/java/com/niit/jdp/repository/Repository.java
@@ -1,7 +1,17 @@
 package com.niit.jdp.repository;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 public interface Repository <T> {
+    List<T> getAll(Connection connection) throws SQLException;
+
+    List<T> getByArtist(Connection connection, String artist);
+
+    List<T> getByGenre(Connection connection, String genre);
+
+    List<T> getBySongName(Connection connection, String songName);
+
+
 }
