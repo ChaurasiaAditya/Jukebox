9207ac89d084b58eb1bc1590c0186e7e8c5857a0
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-20 17:20:39 +0530

    overridden equal() and hashCode() method in Song Class

--- src/main/java/com/niit/jdp/model/Song.java
+++ src/main/java/com/niit/jdp/model/Song.java
@@ -6,6 +6,8 @@
  */
 package com.niit.jdp.model;
 
+import java.util.Objects;
+
 public class Song {
     private int songNumber;
     private String songName;
@@ -72,5 +74,18 @@ public class Song {
 
     public void setGenre(String genre) {
         this.genre = genre;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Song song = (Song) o;
+        return songNumber == song.songNumber && Double.compare(song.duration, duration) == 0 && Objects.equals(songName, song.songName) && Objects.equals(songUrl, song.songUrl) && Objects.equals(artist, song.artist) && Objects.equals(genre, song.genre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(songNumber, songName, songUrl, duration, artist, genre);
     }
 }
