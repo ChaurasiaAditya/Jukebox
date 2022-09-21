774d7233df3d397c71d422467cdc70c914b8cd4e
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-21 11:52:19 +0530

    created add_song6_query.sql

---
+++ src/main/resources/sql/song/add_song6_query.sql
@@ -0,0 +1,3 @@
+-- Add song Light - Sybolism in database
+INSERT INTO `jukebox`.`songs` (`song_number`, `song_name`, `artist`, `duration`, `genre`, `song_url`)
+VALUES ('6', 'Electronomia', 'Sky High', '03:58', 'POP', 'src/main/resources/songs/Elektronomia.wav');
