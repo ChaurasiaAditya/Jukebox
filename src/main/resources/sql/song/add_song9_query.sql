6f1f354b7a213368d36b212647793822b3b5bec8
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-21 11:52:55 +0530

    created add_song9_query.sql

---
+++ src/main/resources/sql/song/add_song9_query.sql
@@ -0,0 +1,3 @@
+-- Add song Lost Sky - Chris Linton in database
+INSERT INTO `jukebox`.`songs` (`song_number`, `song_name`, `artist`, `duration`, `genre`, `song_url`)
+VALUES ('9', 'Lost Sky', 'Chris Linton', '03:14', 'EDM', 'src/main/resources/songs/Lost Sky.wav');
