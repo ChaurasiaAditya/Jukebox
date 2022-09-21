8bc7f95e22b2cc56833035053c82185991cbc621
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-21 11:51:59 +0530

    created add_song4_query.sql

---
+++ src/main/resources/sql/song/add_song4_query.sql
@@ -0,0 +1,3 @@
+-- Add song Heaven - Ehide in database
+INSERT INTO `jukebox`.`songs` (`song_number`, `song_name`, `artist`, `duration`, `genre`, `song_url`)
+VALUES ('4', 'Heaven', 'Ehide ', '04:27', 'POP', 'src/main/resources/songs/Heaven.wav');
