ec796d6efc0f7097b7985a949b53ed2160bbcbb3
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-21 11:52:45 +0530

    created add_song8_query.sql

---
+++ src/main/resources/sql/song/add_song8_query.sql
@@ -0,0 +1,3 @@
+-- Add song Kiss Heaven - Asher Fulero in database
+INSERT INTO `jukebox`.`songs` (`song_number`, `song_name`, `artist`, `duration`, `genre`, `song_url`)
+VALUES ('8', 'Kiss Heaven', 'Asher Fulero', '01:56', 'Classical', 'src/main/resources/songs/Kiss Heavens.wav');
