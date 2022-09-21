64ac972fbab2969f75727deb42bb766de2b8fca4
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-21 11:52:09 +0530

    created add_song5_query.sql

---
+++ src/main/resources/sql/song/add_song5_query.sql
@@ -0,0 +1,3 @@
+-- Add song Fade- Alan walker in database
+INSERT INTO `jukebox`.`songs` (`song_number`, `song_name`, `artist`, `duration`, `genre`, `song_url`)
+VALUES ('5', 'Light', 'Sybolism', '04:51', 'EDM', 'src/main/resources/songs/Light.wav');
