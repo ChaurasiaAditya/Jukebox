837254aa11fe7b670faac51f2fdf7334db5326bd
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-21 11:52:33 +0530

    created add_song7_query.sql

---
+++ src/main/resources/sql/song/add_song7_query.sql
@@ -0,0 +1,3 @@
+-- Add song Janji - Johnning in database
+INSERT INTO `jukebox`.`songs` (`song_number`, `song_name`, `artist`, `duration`, `genre`, `song_url`)
+VALUES ('7', 'Janji', 'Johnning', '03:28', 'EDM', 'src/main/resources/songs/Janji.wav');
