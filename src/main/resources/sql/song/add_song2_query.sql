16193b27765372a4fcaf3039e15148de002bee47
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-21 11:51:38 +0530

    created add_song2_query.sql

---
+++ src/main/resources/sql/song/add_song2_query.sql
@@ -0,0 +1,3 @@
+-- Add song Fade- Alan walker in database
+INSERT INTO `jukebox`.`songs` (`song_number`, `song_name`, `artist`, `duration`, `genre`, `song_url`)
+VALUES ('3', 'Fade', 'Alan Walker', '04:43', 'EDM', 'src/main/resources/songs/Fade.wav');
