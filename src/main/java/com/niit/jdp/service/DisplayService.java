d049af9e0fbbec0c3e93bad746ba0fa7190ddc7d
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-21 16:33:42 +0530

    created DisplayService class

---
+++ src/main/java/com/niit/jdp/service/DisplayService.java
@@ -0,0 +1,14 @@
+/*
+ * Author Name: Aditya Chaurasia
+ * Date: 21-09-2022
+ * Created With: IntelliJ IDEA Community Edition
+ * Profile: github.com/ChaurasiaAditya
+ */
+package com.niit.jdp.service;
+
+import com.niit.jdp.model.Song;
+
+import java.util.List;
+
+public class DisplayService {
+}
