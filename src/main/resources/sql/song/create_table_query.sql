0b0fd5dbaee0d27fb3f723aa9f08f25d85983b08
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-21 11:51:04 +0530

    created_table_query.sql

---
+++ src/main/resources/sql/song/create_table_query.sql
@@ -0,0 +1,11 @@
+-- create a table in the database
+-- add song_number, song_name, artist, genre, duration, song_url
+    CREATE TABLE IF NOT EXISTS `songs`
+    (
+	`song_number` int not null primary key,
+    `song_name` varchar(50) not null,
+    `artist` varchar(50) not null,
+    `genre` varchar(50) not null,
+    `duration` varchar(50) not null,
+    `song_url` varchar(150) not null,
+    );
