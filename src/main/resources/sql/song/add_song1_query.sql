2918859367dc8aad719c476285a965d41bcf48ba
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-21 11:51:26 +0530

    created add_song1_query.sql

---
+++ src/main/resources/sql/song/add_song1_query.sql
@@ -0,0 +1,3 @@
+-- Add song Alone - Alan walker in database
+INSERT INTO `jukebox`.`songs` (`song_number`, `song_name`, `artist`, `duration`, `genre`, `song_url`)
+VALUES ('1', 'Alone', 'Alan Walker', '02:43', 'EDM', 'src/main/resources/songs/Alone.wav');
