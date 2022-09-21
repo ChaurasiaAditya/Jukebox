501263f8c6da4133135229be345da465d3a4c204
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-21 11:53:07 +0530

    created add_song10_query.sql

---
+++ src/main/resources/sql/song/add_song10_query.sql
@@ -0,0 +1,3 @@
+-- Add song Snowfall - Asher Fulero in database
+INSERT INTO `jukebox`.`songs` (`song_number`, `song_name`, `artist`, `duration`, `genre`, `song_url`)
+VALUES ('10', 'Snowfall', 'Asher Fulero', '02:37', 'Classical', 'src/main/resources/songs/Snowfall.wav');
