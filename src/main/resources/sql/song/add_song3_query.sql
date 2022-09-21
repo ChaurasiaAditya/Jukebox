0614b20e032f58e1f30590332afdc29e52a03b61
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-21 11:51:49 +0530

    created add_song3_query.sql

---
+++ src/main/resources/sql/song/add_song3_query.sql
@@ -0,0 +1,3 @@
+-- Add song Fade- Alan walker in database
+INSERT INTO `jukebox`.`songs` (`song_number`, `song_name`, `artist`, `duration`, `genre`, `song_url`)
+VALUES ('3', 'Cartoon', 'Daniel Levi', '03:27', 'POP', 'src/main/resources/songs/Cartoon.wav');
